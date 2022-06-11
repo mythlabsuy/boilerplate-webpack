423bad3c73c9e7f4697e1d541cdaebd86c1dae1b
Damian <damsch12@gmail.com>
2022-06-11 11:07:45 -0300

    i18n WIP

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,4 +7,27 @@ console.log(
   'background: #e4b349; border: 1px solid #000; padding: 4px; padding-top: 10px; padding-bottom: 8px; color:#21557d;'
 );
 
+import Handlebars from 'handlebars';
+import i18next from 'i18next';
+import registerI18nHelper from 'handlebars-i18next';
+
 // add custom js below
+i18next.init({
+  resources: {
+    en: {
+      translation: {
+        "key": "hello world"
+      }
+    },
+    es: {
+      translation: {
+        "key": "hola mundo"
+      }
+    }
+  }
+}, function(err, t) {
+  // initialized and ready to go!
+  document.getElementById('output').innerHTML = i18next.t('key');
+});
+
+registerI18nHelper(Handlebars, i18next);
